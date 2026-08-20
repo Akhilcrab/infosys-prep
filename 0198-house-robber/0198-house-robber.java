ac8370f4569acbb4839b9d913159967ee3644bc2
Akhil Rawat <akhilrwt2004@gmail.com>
2026-08-20 20:28:10 +0530

    Time: 0 ms (100%), Space: 42.5 MB (95.02%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length ;
+          
+        int prev1 = 0;
+        int prev2 = 0;
+        for( int i = 0 ; i < n ; i++){
+
+            int take =  nums[i] + prev2;
+            int skip =  prev1;
+
+            int current = Math.max(take ,skip);
+
+            prev2 = prev1;
+            prev1 = current ;
+        }
+        return prev1;
+    }
+}
